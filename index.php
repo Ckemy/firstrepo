7688057cc1de89ec1963f2a0a3b21a4411274fcc
Chlo <pavadayc@gmail.com>
2018-07-27 16:44:12 +0200

    ajout index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>First document</title>
+</head>
+<body>
+	<article>
+		<header>
+		<h1>Article</h1>
+		<p>Sous article</p>
+		</header>
+		<main>
+			<section>
+			Du texte
+			</section>
+		</main>
+		<footer>
+		</footer>
+	</article>
+</body>
+</html>
